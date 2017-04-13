64af9bea731ad07821a5a6b9444952ca868dcb6e
mnichko <michal.kowalewski@yahoo.pl>
2017-04-13 02:10:56 +0200

    Dodano obsługę obrotów kamery wraz z robieniem zdjęć. Dodano i poprawiono komentarze, poprawiono nazwy, ułożono białe znaki.

--- service.py
+++ service.py
@@ -1,98 +1,114 @@
 from flask import Flask
 import os
-from usb import usb
+from usb import Usb
 app = Flask(__name__)
-arduino_connect = usb()
+arduino_connect = Usb()
+
+# Tymczasowo Arduino ignoruje przesyłane wartości przemieszczeń i kątów, więc są podawane dowolne.
+# Tymczasowo nie można założyć, że Arduino zwraca informacje o dokonanym przemieszczeniu lub obrocie,
+# więc nie sprawdzamy poprawności ruchu.
+# Można za to założyć, że ruch zostanie przerwany przy zbliżaniu się do przeszkody.
+
 
 @app.route('/forward')
-def forward(): # przesyla polecenie jazdy do przodu (f) i dystans (w metrach), nastepnie czeka na odpowiedz
+def forward():  # Przesyla polecenie jazdy do przodu (f) i dystans (w metrach), nastepnie czeka na odpowiedz
     print("jedziemy do przodu")
 
     # implementacja
-    distance = "10" # tymczasowo dystans jest staly i ignowrowany
+    distance = "10"  # tymczasowo dystans jest staly i ignowrowany
     orders = ["f", distance]
     arduino_connect.send(orders)
     status = arduino_connect.receive()
-    print("chuj",status) # status nie jest sprawdzany, zatem sie go tylko wypisuje
+    print(status)  # status nie jest sprawdzany, zatem sie go tylko wypisuje
+    # Kod czeka na lepsze czasy:
     # if status != distance:
-       #  print("Error")
+    #   print("Error")
     # else:
-        # print("Ok")
+    #   print("Ok")
     return "forward"
 
+
 @app.route('/left')
-def left(): # przesyla polecenie skretu w lewo (l) i kat (w stopniach), nastepnie czeka na odpowiedz
+def left():  # przesyla polecenie skretu w lewo (l) i kat (w stopniach), nastepnie czeka na odpowiedz
     print("skrecamy w lewo")
-	
-    #implementacja
-    angle = "90" # tymczasowo kat jest staly i ignowrowany
+    # implementacja
+    angle = "90"  # tymczasowo kat jest staly i ignowrowany
     orders = ["l", angle]
     arduino_connect.send(orders)
     status = arduino_connect.receive()
-    print(status) # status nie jest sprawdzany, zatem sie go tylko wypisuje 
-	
-    return ""
+    print(status)  # status nie jest sprawdzany, zatem sie go tylko wypisuje
+    return "left"
+
 
 @app.route('/right')
-def right(): # przesyla polecenie skretu w lewo (l) i kat (w stopniach), nastepnie czeka na odpowiedz
+def right():  # przesyla polecenie skretu w lewo (l) i kat (w stopniach), nastepnie czeka na odpowiedz
     print("skrecamy w prawo")
-    angle = "90" # tymczasowo kat jest staly i ignowrowany
+    angle = "90"  # tymczasowo kat jest staly i ignowrowany
     orders = ["r", angle]
     arduino_connect.send(orders)
     status = arduino_connect.receive()
-    print(status) # status nie jest sprawdzany, zatem sie go tylko wypisuje
-    return ""
+    print(status)  # status nie jest sprawdzany, zatem sie go tylko wypisuje
+    return "right"
+
 
 @app.route('/backward')
-def backward(): # przesyla polecenie jazdy do tylu (b) i dystans (w metrach), nastepnie czeka na odpowiedz
+def backward():  # przesyla polecenie jazdy do tylu (b) i dystans (w metrach), nastepnie czeka na odpowiedz
     print("jedziemy do tylu")
 
     # implementacja
-    distance = "10" # tymczasowo dystans jest staly i ignowrowany
+    distance = "10"  # tymczasowo dystans jest staly i ignowrowany
     orders = ["b", distance]
     arduino_connect.send(orders)
     status = arduino_connect.receive()
-    print(status) # status nie jest sprawdzany, zatem sie go tylko wypisuje
-    return ""
+    print(status)  # status nie jest sprawdzany, zatem sie go tylko wypisuje
+    return "backward"
+
 
 @app.route('/p')
-def serwop(): # przesyla polecenie skretu w lewo (l) i kat (w stopniach), nastepnie czeka na odpowiedz
+def camera_right():  # przesyla polecenie skrętu serwa kamery w prawo (p) i kat (w stopniach),
+    #  nastepnie czeka na odpowiedź
     print("serwo w prawo")
 
-    #implementacja
-    angle = "0" # tymczasowo kat jest staly i ignowrowany
+    # implementacja
+    angle = "0"  # tymczasowo kat jest staly i ignowrowany
     orders = ["p", angle]
     arduino_connect.send(orders)
     status = arduino_connect.receive()
-    print(status) # status nie jest sprawdzany, zatem sie go tylko wypisuje
+    print(status)  # status nie jest sprawdzany, zatem sie go tylko wypisuje
 
     return ""
 
-@app.route('/q')
-def serwoq(): # przesyla polecenie skretu w lewo (l) i kat (w stopniach), naste$
-    print("serwo w lewo")
 
-    #implementacja
-    angle = "0" # tymczasowo kat jest staly i ignowrowany
+@app.route('/q')
+def camera_left():  # przesyla polecenie skretu serwa kamery w lewo (q) i kat (w stopniach),
+    # następnie czeka na odpowiedź
+    print("Obrót kamery w lewo")
+
+    # implementacja
+    angle = "0"  # tymczasowo kat jest staly i ignorowany
     orders = ["q", angle]
     arduino_connect.send(orders)
     status = arduino_connect.receive()
-    print(status) # status nie jest sprawdzany, zatem sie go tylko wypisuje
+    print(status)  # status nie jest sprawdzany, zatem sie go tylko wypisuje
 
     return ""
+
 
 @app.route('/s')
-def serwos(): # przesyla polecenie skretu w lewo (l) i kat (w stopniach), naste$
-    print("serwo w prawo")
+def get_data_from_sensors():  # przesyla polecenie pobrania danych z sensorów(s),
+    # następnie czeka na odesłanie danych ze wszystkich sensorów.
+    # Obecnie wypisuje tylko wszystko nieprzetworzone.
+    # W przyszłości trzeba dodać opcję pobierania tych pomiarów do celów sterowania autonomicznego.
+    print("Pobierz dane z sensorów")
 
-    #implementacja
-    angle = "0" # tymczasowo kat jest staly i ignowrowany
-    orders = ["s", angle]
+    # implementacja
+    angle = "0"  # tymczasowo kat jest staly i ignorowany
+    orders = ["s", angle]  # Nie mam pojęcia, czy to konieczne. Ktoś to chyba przekopiował.
     arduino_connect.send(orders)
     status = arduino_connect.receive()
-    print(status) # status nie jest sprawdzany, zatem sie go tylko wypisuje
+    print(status)  # status nie jest sprawdzany i przetwarzany ani formatowany, zatem sie go tylko wypisuje
 
-    return ""
+    return status
 
 
 @app.route('/test')
@@ -100,26 +116,48 @@ def test():
     orders = ["p"]
     arduino_connect.send(orders)
     status = arduino_connect.receive()
-    print(status) # status nie jest sprawdzany, zatem sie go tylko wypisuje
+    print(status)  # status nie jest sprawdzany, zatem sie go tylko wypisuje
     return ""
+
+
+@app.route('/single')
+def single_shot(name="test"):  # wykonuje pojedyncze zdjecie i zapisuje je w katalogu static,
+    # pod nazwą podaną jako argument -- string name
+    print("robimy zdjecia")
+    # command = "raspistill -n -t 1 -o static/"+name+".jpg"  # sekundowe opóźnienie
+    command = "raspistill -n -t -o static/"+name+".jpg"  # natychmiast
+
+    os.system(command)  # wywołujemy raspistill który wykonuje zdjęcie
+    print("zrobiono zdjecie")
+    return "zz"  # zdjecie zrobione
 
 
 @app.route('/camera')
-def camera():
-    print("robimy zdjecia")
-    os.system("raspistill -n -t 1 -o static/test.jpg") #wywołujemy raspistill który wykonuje zdjęcie po sekundzie
-    print("zrobiono zdjecie")
-    return "zz" # zdjecie zrobione
+def round_camera(turns=10):  # Wykonuje pełen obrót kamerą i zdjęcia wokół
+    # Zapisuje je jako pliki 0.jpg do 9.jpg z kolejnymi numerami
+    # Zdjęcia nadpisują poprzednie!
 
-@app.route('/photos')
+    # Póki co, czekam aż Arduino zacznie zwracać i przyjmować kąty, o jakie kamera się obróciła.
+    # Do tego czasu tymczasowe rozwiązanie.
+    # NIEPRZETESTOWANE na robocie
+    for i in range(turns):
+        name = str(i)
+        single_shot(name)  # natychmiast wykonaj i zapisz zdjęcie.
+        right()  # może być i left(), jeśli to będzie miało znaczenie
+
+
+@app.route('/photos')  # Pokazuje linki do zdjęć, można zrobić coś sprytniejszego
 def photos():
-    print("zdjecia <\br>")
+    print("Pojedyncze: <\\br>")
     print('<a href="static/test.jpg"> Tu </a>')
-    #zdjęcia dostępne pod /static/test.jpg
-    #implementacja
+    print("Pełne koło: <\\br>")
+    for i in range(10):
+        name = str(i)
+        print('<a href="static/'+name+'.jpg">'+name+'</a>')
+    # implementacja
     return ""
+
 
 if __name__ == '__main__':
     arduino_connect.open()
     app.run(host='0.0.0.0')
-
--- usb.py
+++ usb.py
@@ -1,29 +1,27 @@
 import serial
 
-class usb:
+
+class Usb:
     ser = serial.Serial
-    def open(self): # otwiera polaczenie;stale wartosci, mozna sparametryzowac
-        self.ser = serial.Serial ("/dev/ttyACM0")    #Open named port
-        self.ser.baudrate = 9600                     #Set baud rate to 9600
 
+    def open(self):  # otwiera polaczenie;stale wartosci, mozna sparametryzowac
+        self.ser = serial.Serial("/dev/ttyACM0")    # Domyślne urządzenie
+        self.ser.baudrate = 9600                     # Domyślne wartości
 
-    def write(self, order): # Pisze na usb pojedyncza linie
+    def write(self, order):  # Pisze na usb pojedyncza linie
         self.ser.write(order.encode())
 
-
-    def send(self, orders): # wysyla liste orders z dodanym znakiem konca "$"
+    def send(self, orders):  # wysyla liste orders z dodanym znakiem konca "$"
         for i in orders:
             self.write(i)
-        self.write('E') # koniec polecenia
+        self.write('E')  # koniec polecenia
 
-
-    def read(self): # czeka i zwraca pojedyncza odpowiedz (linie)
+    def read(self):  # czeka i zwraca pojedyncza odpowiedz (linie)
         data = self.ser.readline()
         print(data)
         return data.decode()  # Zwraca odpowiedz przekonwertowana na stringa
 
-
-    def receive(self): # czeka, wczytuje do "$" i zwraca wszysstkie komunikaty jako liste (bez "$").
+    def receive(self):  # czeka, wczytuje do "$" i zwraca wszysstkie komunikaty jako liste (bez "$").
         data = []
         last = self.read()
         while last != 'E\r\n':
@@ -33,7 +31,5 @@ class usb:
         print("Koniec")
         return data
 
-
-
-    def close(self): # zamkniecie polaczenia po usb
-       self.ser.close()
+    def close(self):  # zamkniecie polaczenia po usb
+        self.ser.close()
